3ee0cc2b80ae180ae1077172df96c37cd97e133b
your_username <your_email@gmail.com>
2023-03-16 14:33:02 -0400

    CountDownTimer

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# PythonCountdownTimer
+# Python Countdown Timer
+
+'countdowntimer.py' is a python program that creates a timer in the Debugger. The program counts down in seconds, once it hits 0 time, it prints a "Stop" message. 
+
+
---
+++ countdowntimer.py
@@ -0,0 +1,12 @@
+import time
+# create a python program to create a countdown timer   
+def countdown(time_sec):
+    while time_sec:
+        mins, secs = divmod(time_sec, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins,secs)
+        print(timeformat) #, end='\r')
+        time.sleep(1)
+        time_sec -= 1
+    print ("Stop")
+timecount=int(input("Set your Timer in Seconds: "))
+countdown(timecount)
